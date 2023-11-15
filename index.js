b0017e6c869ceb99c55f34d63125740812853705
Mrinal <62418152+mrinalsarkar1999@users.noreply.github.com>
2023-11-15 21:43:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-  // $().fadeIn(900);
-
+  $("#name, #hi").slideDown(1000);
+  //Animate the main content after a timeout of 1 second which includes the nav elements, and the summary
   setTimeout(() => {
     $(".nav-ul").slideDown(1000).css("display", "flex");
-    $(".profile>img").fadeIn(1000);
     $("#quote").stop(true, true).animate(
       {
         height: "toggle",
@@ -18,9 +17,13 @@ $(document).ready(function () {
       },
       1000
     );
-  }, 2000);
+  }, 1500);
+  //Animate the profile image after the main content animation is complete
+  setTimeout(() => {
+    $(".profile>img").fadeIn(1000);
+  }, 2500);
 
-  // $(".viewport-header").slideDown(1000);
+  // adding click events to the navigation bar elements(About, Contact and shop)
   $("#contact-link").click(function () {
     $("#contact-link").attr("href", "#contact");
     $(".page-footer").css("display", "flex");
@@ -28,11 +31,14 @@ $(document).ready(function () {
   });
   $("#about-link").click(function () {
     $("#about-link").attr("href", "#about");
-    $(".about").fadeIn(800).css("display", "flex");
+    $(".about").fadeIn(1500).css("display", "flex");
   });
   $("#shop-link").click(function () {
     $("#shop-link").attr("href", "#shop");
+    //On click of shop
     $(".shop").css("display", "flex");
+    //Below code calls the jsonplaceholder url that returns a json array of objects containing about 500 photos out of which
+    //using a for statement we only take the first 6 photos and display them by adding innerhtml to a div under Shop heading
     $.ajax({
       url: "https://jsonplaceholder.typicode.com/photos",
       method: "get",
