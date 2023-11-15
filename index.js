6ce788da89dc830492cd314ff305420c05d117e7
Mrinal <62418152+mrinalsarkar1999@users.noreply.github.com>
2023-11-15 21:01:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
+  // $().fadeIn(900);
+  $(".nav-ul").slideDown(1000).css("display", "flex");
+  $(".profile>img").fadeIn(1000);
+  $("#quote").stop(true, true).animate(
+    {
+      height: "toggle",
+      opacity: "toggle",
+    },
+    1000
+  );
+  $("#summary").stop(true, true).animate(
+    {
+      height: "toggle",
+      opacity: "toggle",
+    },
+    1000
+  );
+
+  // $(".viewport-header").slideDown(1000);
   $("#contact-link").click(function () {
     $("#contact-link").attr("href", "#contact");
     $(".page-footer").css("display", "flex");
@@ -6,7 +25,7 @@ $(document).ready(function () {
   });
   $("#about-link").click(function () {
     $("#about-link").attr("href", "#about");
-    $(".about").css("display", "flex");
+    $(".about").fadeIn(400).css("display", "flex");
   });
   $("#shop-link").click(function () {
     $("#shop-link").attr("href", "#shop");
